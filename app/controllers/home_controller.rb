8419f32c905b7b48d929b7ba75b79e06b982be13
Krystal <krystal.raysor@gmail.com>
2020-01-18 03:37:07 +0000

    Added cryptocompare-crytopian 'Price-Multiple Symbols Full Data' api and used same format as 'Latest News Articles' to add Price data. Enclosed price data in Bootstrap-Tables and organized index.html.erb file to neatly seperate price data from news.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,18 @@ class HomeController < ApplicationController
     require 'net/http'
     require 'json'
 
+    # Pulls in News Data from cryptocompare
     @url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @news = JSON.parse(@response)
+
+    # Pulls in Price Data from cryptocompare
+    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,BSV,USDT,LTC,EOS,BNB,XLM<&tsyms=USD'
+    @prices_uri = URI(@prices_url)
+    @prices_response = Net::HTTP.get(@prices_uri)
+    @prices = JSON.parse(@prices_response)
+
   end
 
   def prices
